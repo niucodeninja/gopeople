8f2fb9b1770436b639fce79ccb7805c33b472b6b
Jorge Osorio <niucodeninja@gmail.com>
2012-05-18 11:05:49 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gopeople::Application.config.secret_token = '9febef71f28c4f3a271b7e3f32c8a004f59782066ba0e781ee12a779b1d69a6bdb2a4c9bd850a5f74d534c9cfc9f28162e3f1780bd487541e96de5ed61d94d66'
