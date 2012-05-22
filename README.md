8698ac17157bfded6cc414da397b4859ff0a0e9d
Jorge Osorio <niucodeninja@gmail.com>
2012-05-22 18:45:58 -0500

    Custom registration controller

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-gopeople
+Go People
 ========
 
-Go People
+A social network to share places and win a status to go there or buy coupons.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Gopeople::Application.routes.draw do
+  get "registrations/create"
+
   get "friendships/create"
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
